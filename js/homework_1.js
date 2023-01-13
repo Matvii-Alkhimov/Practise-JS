f4b9ab09bbb969ecb1a6f17203997d0aeb265495
Matvii-Alkhimov <112165072+Matvii-Alkhimov@users.noreply.github.com>
2023-01-13 19:52:21 +0200

    Додає Джаву

---
+++ js/homework_1.js
@@ -0,0 +1,87 @@
+// 1
+
+const str = 'Привіт';
+const num = 123;
+const flag = true;
+const txt = 'true';
+
+console.log( typeof str);
+console.log( typeof num);
+console.log( typeof flag);
+console.log( typeof txt);
+
+// 2
+
+const favouriteFruit = 'apple';
+const number = 10;
+const letter = 'a';
+const time = '7am';
+const isOnline = true;
+
+let myAge = 15;
+let deadline = 'Friday';
+let secondNumber = 4;
+let zero = false;
+let answer = 'true';
+
+// 3
+
+const question = prompt("Введіть ваше ім'я");
+console.log(question);
+
+// 4
+
+const MyAge = 15;
+const myFriendAge = 14;
+const agesSummary = MyAge + myFriendAge;
+alert(agesSummary);
+
+// 5
+
+const a = 8;
+const b = 7;
+const c = a + b;
+console.log(c);
+
+// 6
+
+const a1 = 5 + 3;
+const a2 = 3 - 5;
+const a3 = 5 * 3;
+const a4 = 5 / 3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+
+// 7
+
+const a6 = 5 % 3;
+const a7 = 3 % 5;
+const a8 = 5 - '3';
+const a9 = '5' - 3;
+const a10 = 75 + 'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+// 8
+
+const h = 23;
+const w = 10;
+const S = h * w;
+
+console.log(S);
+
+// 9
+
+const hC = 10;
+const dC = 4;
+const V = hC * dC;
+
+console.log(V)
